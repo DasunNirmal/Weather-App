d37df9adf1770e9e42c276a083e007387006a91f
DasunNirmal <dasun200301@gmail.com>
2025-03-31 15:59:30 +0530

    main container half done

---
+++ src/components/ShowWeather.tsx
@@ -0,0 +1,10 @@
+export const ShowWeather = () => {
+    return (
+        <div className='min-h-screen relative'>
+            <div className='bg-white/50 w-[400px] h-[650px] rounded-[12px] p-8 shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)] text-black/80
+            bg-blend-overlay flex justify-between items-center flex-col absolute top-40 left-0 right-0 bottom-0 mx-auto'>
+                <h1>Weather</h1>
+            </div>
+        </div>
+    );
+};
